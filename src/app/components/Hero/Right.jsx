38ffeb2ee873a7f3670c16b9da7a575addd89225
Leonardo Timkang <locodafux@gmail.com>
2025-03-29 18:29:39 +0800

    fixed skills mobile

--- src/app/components/Hero/Right.jsx
+++ src/app/components/Hero/Right.jsx
@@ -1,6 +1,6 @@
 const Right = ({ isMobile, Banner}) => {
     return (
-      <div className={`${isMobile ? "w-screen h-[400px] " : "w-1/2 ml-[-250px]"} flex justify-center items-center`}>
+      <div className={`${isMobile ? "w-screen h-[400px] " : "w-1/2 ml-[-250px]"} flex place-items-center`}>
         <Banner path="/Banner.png" alt="Image" className="w-full h-full object-cover" />
       </div>
     )
--- src/app/components/Skills.jsx
+++ src/app/components/Skills.jsx
@@ -38,10 +38,13 @@ const Skills = () => {
   ]
 
   return(
-    <div id = "skills" className = "flex flex-col items-center flex-1 min-h-screen w-screen p-12">
-      <Title first="My" second="Skills"/>
+    <div id = "skills" className = "flex flex-col items-center flex-1 lg:min-h-screen w-screen pb-12 ">
 
-      <div className="grid grid-cols-4 gap-12 w-full max-w-6xl mt-36">
+      <div className="w-screen flex justify-center gap-x-3 lg:text-5xl  text-4xl">
+        <span >My</span> <span className = " font-bold">Skills</span>
+      </div>
+
+      <div className="mt-12 grid lg:grid-cols-4 gap-12 w-full max-w-6xl  place-items-center">
         {
           skillList.map((item, index) => (
             <SkillCard key={index} id={item.id} name={item.name} alt="icon"/>
