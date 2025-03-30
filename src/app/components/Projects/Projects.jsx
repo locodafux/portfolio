7f2bae425de522580cd99531d5b6496ef7b579ea
Leonardo Timkang <locodafux@gmail.com>
2025-03-30 19:23:46 +0800

    UPDATE-Projects

--- src/app/components/Projects/Projects.jsx
+++ src/app/components/Projects/Projects.jsx
@@ -5,21 +5,21 @@ const Projects = (props) => {
   const projects = [
     {
       path: "/projects/project-1.png",
-      alt: "Project 1",
-      title: "Crypto Screener Application",
-      desc: "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
+      alt: "Petty Cash Replenishment",
+      title: "Petty Cash Replenishment",
+      desc: "Created using LARAVEL 7, MYSQL VUE.JS. Petty Cash Replenishment is a project that is aim to be used by all of the branches of Wilcon. They will use this to request for petty cash that is going to be approved by several higher ups while maintaining records, utilizing api to save into internal software that is used across all Wilcon Branches."
     },
     {
       path: "/projects/project-1.png",
-      alt: "Project 1",
-      title: "Crypto Screener Application",
-      desc: "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
+      alt: "Attendance Management System",
+      title: "Attendance Management System",
+      desc: "Created using Vanilla Javascript, Tailwind, PHP, MYSQL.Attendance Application where the Employees / Interns of Erovoutika Inc. will time in and time out. To time in the need to input their employee Id and Log in. And by logging in they will see their own profile showing the record of their attendance"
     },
     {
       path: "/projects/project-1.png",
-      alt: "Project 1",
-      title: "Crypto Screener Application",
-      desc: "I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to specimen book."
+      alt: "Thesis",
+      title: "APyro:  SMART ATTENDANCE SYSTEM FOR HYFLEX LEARNING IN HIGHER EDUCATION USING CONVOLUTIONAL NEURAL NETWORK ALGORITHM",
+      desc: "APyro is a thesis project built with ASP.NET, SQL Server, and Bootstrap. It uses machine learning for attendance monitoring by verifying students' identities via facial recognition. The system has admin, student, and teacher interfaces, where teachers start live sessions, and students are marked absent if not detected in front of the camera, even if logged in."
     },
   ] 
 
