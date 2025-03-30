15fdfe41038afbcf286e4bc12abf2fa84dc22b92
Leonardo Timkang <locodafux@gmail.com>
2025-03-30 18:38:15 +0800

    UPDATE : Contact

--- src/app/components/Contact/Right.jsx
+++ src/app/components/Contact/Right.jsx
@@ -10,7 +10,7 @@ const Right = () => {
       <p className="text-zinc-500 w-full">I seek to push the limits of creativity to create high-engaging, user-friendly, and memorable interactive experiences.</p>
       <div className="flex flex-col gap-2">
         <span className="text-lg lg:text-3xl font-semibold">leonardotimkang@gmail.com</span>
-        <span className="ltext-lg g:text-3xl font-semibold">09260919299</span>
+        <span className="text-lg lg:text-3xl font-semibold">09260919299</span>
       </div>
     </div>
   )
